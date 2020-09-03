99c0a6a8ac3dd964526d7ec52556e1c7aced1ab0
bhargavk156 <bhargavk156@gmail.com>
2020-09-03 16:02:58 -0400

    reactjs projects

---
+++ redux2/src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <header>
+            <div>
+               <p>&copy; DeveloperFunnels</p>
+            </div>
+        </header>
+    )
+}
+
+export default Footer;
